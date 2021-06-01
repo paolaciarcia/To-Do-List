3f3efa1d881fdc7d12a75cca16fd287ffd6bba23
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-06-01 10:21:56 -0300

    Add Data Model

---
+++ ToDoList/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Title.swift
+//  ToDoList
+//
+//  Created by Erick Borges on 01/06/21.
+//
+
+import Foundation
+
+struct Item {
+    var title: String = ""
+    var done: Bool = false
+}
--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -9,14 +9,33 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    @IBOutlet weak var addNewiTEM: UIBarButtonItem!
+    @IBOutlet weak var addNewItem: UIBarButtonItem!
     
+    var itemArray = [Item]()
     
-    var itemArray = ["Study", "Go to gym", "Watch TV", "Talk with mom"]
-    
+    //1 - persistir dados
+    let userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var newItem = Item()
+        newItem.title = "Shopping"
+        itemArray.append(newItem)
+        
+        var newItem2 = Item()
+        newItem2.title = "Buy"
+        itemArray.append(newItem2)
+        
+        var newItem3 = Item()
+        newItem3.title = "do something"
+        itemArray.append(newItem3)
+       
+        
+        //4 - persistir dados
+//        if let items = userDefaults.array(forKey: "ToDoListArray") as? [Item] {
+//            itemArray = items
+//        }
     }
     
     @IBAction func addButton(_ sender: UIBarButtonItem) {
@@ -34,12 +53,17 @@ class ToDoListViewController: UITableViewController {
             print("success")
             print(tf.text!)
             
+            var newItem = Item()
+            newItem.title = tf.text!
+            
             if tf.text != "" {
-                self.itemArray.append(tf.text!)
+                self.itemArray.append(newItem)
             } else {
-                self.itemArray.append("New item")
+                self.itemArray.append(Item())
             }
             
+            //2 - persistir dados | 3 - AppDelegate
+            self.userDefaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
     
@@ -58,27 +82,22 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
         
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+
         return cell
     }
     
     //MARK: - TableViewDelegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    
-        let selectedRow = tableView.cellForRow(at: indexPath)
-
-        switch selectedRow?.accessoryType {
-        case .checkmark:
-            selectedRow?.accessoryType = .none
-        default:
-            selectedRow?.accessoryType = .checkmark
-        }
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
-      
     }
-    
 }
-
